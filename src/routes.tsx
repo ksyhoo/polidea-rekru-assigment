bca85579d6fbbf79250a7ad620c1eb0c6f8b7b80
Krzysztof Kulikowski <krzysztof.kulikowski@netguru.com>
2020-09-20 10:52:25 +0200

    Addes routes with lazy loading

---
+++ src/routes.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+const Router = {
+  Landing: React.lazy(() => import('pages/Landing')),
+  Repository: React.lazy(() => import('pages/Repository')),
+};
+
+export const Routes: React.SFC = () => (
+  <BrowserRouter>
+    <React.Suspense fallback={<p>Loading</p>}>
+      <Switch>
+        <Route path="/" component={Router.Landing} />
+        <Route path="/repository" component={Router.Repository} />
+      </Switch>
+    </React.Suspense>
+  </BrowserRouter>
+);
