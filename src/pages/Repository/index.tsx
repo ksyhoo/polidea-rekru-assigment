559f8a406b792070360a3673d5e3dafc0e53c74f
Krzysztof Kulikowski <krzysztof.kulikowski@netguru.com>
2020-09-20 18:04:57 +0200

    Added fetch details page action

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { fetchRepository } from 'store/searchRepositories';
+import RepositoryDetailsPanel from 'components/RepositoryDetailsPanel';
 
-const RepositoryDetails: React.SFC = () => <p>repository</p>;
+const RepositoryDetails: React.SFC = () => {
+  const dispatch = useDispatch();
+  const { repo, owner } = useParams();
+
+  useEffect(() => {
+    dispatch(fetchRepository(repo, owner));
+  }, [dispatch, repo, owner]);
+
+  return <RepositoryDetailsPanel />;
+};
 
 export default RepositoryDetails;
