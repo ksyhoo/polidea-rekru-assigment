06d3c58bda72ef396869028c2476d836e13f6819
Krzysztof Kulikowski <krzysztof.kulikowski@netguru.com>
2020-09-20 10:52:09 +0200

    Updated main App componets with provides , routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { Routes } from './routes';
+import store from 'store';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import theme from 'theme';
+import { GlobalStyles } from 'utils/styled';
 
-const  App= () => <p>Setup</p>
+const App: React.FC = () => (
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <>
+        <GlobalStyles />
+        <Routes />
+      </>
+    </Provider>
+  </ThemeProvider>
+);
 
 export default App;
