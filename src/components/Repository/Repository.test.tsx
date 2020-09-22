b9d7060c6eb8568e461bd5a778be1e86af3d7d1d
Krzysztof Kulikowski <krzysztof.kulikowski@netguru.com>
2020-09-22 17:14:45 +0200

    Added basic react testing library test

--- src/components/Repository/Repository.test.tsx
+++ src/components/Repository/Repository.test.tsx
@@ -5,26 +5,36 @@ import 'jest-styled-components';
 import { ThemeProvider } from 'styled-components';
 import theme from 'theme';
 import { BrowserRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+
+const repo = {
+  name: 'repo',
+  owner: { name: ' owner', type: 'any' },
+  numberOfStars: 22,
+  primaryLanguage: 'Ponglish',
+  id: 1,
+};
+
+const Component = () => (
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <Repo repo={repo} />
+    </ThemeProvider>
+  </BrowserRouter>
+);
 
 describe('RepoComponent', () => {
   it('render Repo component', async () => {
-    const wrapper = create(
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <Repo
-            repo={{
-              name: 'repo',
-              owner: { name: ' owner', type: 'any' },
-              numberOfStars: 22,
-              primaryLanguage: 'Ponglish',
-              id: 1,
-            }}
-          />
-        </ThemeProvider>
-      </BrowserRouter>
-    ).toJSON();
+    const wrapper = create(<Component />).toJSON();
 
     await Repo;
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('shows the children when the checkbox is checked', () => {
+    const testMessage = repo.name;
+    render(<Component />);
+    expect(screen.getByText(testMessage)).toBeInTheDocument();
   });
 });
