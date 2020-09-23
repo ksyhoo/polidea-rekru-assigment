f01e7abac7837aef67e0b1a26294acb5ff07d8f5
Krzysztof Kulikowski <krzysztof.kulikowski@netguru.com>
2020-09-23 18:52:26 +0200

    Added typin, styling

--- src/components/Hoc/WithLoading.tsx
+++ src/components/Hoc/WithLoading.tsx
@@ -1,22 +1,25 @@
 import React, { useState } from 'react';
 import ClipLoader from 'react-spinners/ClipLoader';
-import { WithLoadingContainer } from './styled';
+import { WithLoadingContainer, LoadingWrapper } from './styled';
 
-const WithLoading = (Component: any) => {
-  return function WrappedWithLoading(props: any): any {
+const WithLoading = (
+  Component: React.SFC<{ setLoading: (isLoading: boolean) => void }>
+) => {
+  return function WrappedWithLoading(props: {
+    isLoading?: boolean;
+  }): JSX.Element {
     const [isLoading, setIsLoading] = useState(false);
     const setLoading = (isComponentLoading: boolean) =>
       setIsLoading(isComponentLoading);
-
     return (
-      <>
+      <LoadingWrapper>
+        <Component {...props} setLoading={setLoading} />
         {isLoading && (
           <WithLoadingContainer>
-            <ClipLoader size={150} loading={props.isLoading} />
+            <ClipLoader size={150} loading={isLoading} />
           </WithLoadingContainer>
         )}
-        <Component {...props} setLoading={setLoading} />
-      </>
+      </LoadingWrapper>
     );
   };
 };
--- src/components/Hoc/styled.tsx
+++ src/components/Hoc/styled.tsx
@@ -4,4 +4,17 @@ export const WithLoadingContainer = styled.div`
   display: flex;
   justify-content: center;
   width: 100%;
+  height: 100%;
+  background: transparent;
+  position: absolute;
+  top: 20%;
+  left: 0%;
+`;
+
+export const LoadingWrapper = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
 `;
